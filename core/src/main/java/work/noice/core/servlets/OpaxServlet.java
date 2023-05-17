2d2f2e4683049478b597b97cf30009c132f37698
dipsyy <dipankar_2590@yahoo.co.in>
2023-05-17 17:47:51 +1000

    Update OpaxServlet.java

--- core/src/main/java/work/noice/core/servlets/OpaxServlet.java
+++ core/src/main/java/work/noice/core/servlets/OpaxServlet.java
@@ -2,6 +2,7 @@ package work.noice.core.servlets;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpResponse;
@@ -27,12 +28,16 @@ import work.noice.core.services.OpaxService;
 import javax.servlet.Servlet;
 import java.io.IOException;
 
-@Component(immediate = true, service = Servlet.class, property = {
-        Constants.SERVICE_DESCRIPTION + "=ChatGPT Integration",
-        "sling.servlet.methods=" + HttpConstants.METHOD_POST,
-        "sling.servlet.paths=" + "/bin/chat",
-        "sling.servlet.extensions={\"json\"}"
-})
+@Component(
+        immediate = true,
+        service = Servlet.class,
+        property = {
+                Constants.SERVICE_DESCRIPTION + "=ChatGPT Integration",
+                "sling.servlet.methods=" + HttpConstants.METHOD_POST,
+                "sling.servlet.paths=" + "/bin/chat",
+                "sling.servlet.extensions={\"json\"}"
+        }
+)
 public class OpaxServlet extends SlingAllMethodsServlet {
 
     private static final Logger Logger = LoggerFactory.getLogger(OpaxServlet.class);
@@ -47,46 +52,50 @@ public class OpaxServlet extends SlingAllMethodsServlet {
 
     @Override
     protected void doPost(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException {
-        Logger.error("@@@@@@@@@@@@@@@ inside Post");
         String body = IOUtils.toString(request.getReader());
-        Logger.error("@@@@@@@@@@@@@@@ inside Post {}", body);
-
         if (StringUtils.isNotBlank(body)) {
-            String prompt = generatePrompt(body.replaceAll("%20", " "));
-            Logger.error("@@@@@@@@@@@@@@@ PROMPT {}", prompt);
-            String message = generateMessage(prompt);
-
-            response.setCharacterEncoding("UTF-8");
-            response.getWriter().write(message);
+            String prompt = generatePrompt(body);
+            if (StringUtils.isNotBlank(prompt)) {
+                String result = generateMessage(prompt);
+                if (StringUtils.isNotBlank(result)) {
+                    JsonObject jsonObject = new JsonObject();
+                    jsonObject.addProperty("data", result);
+                    response.setStatus(200);
+                    response.setCharacterEncoding("UTF-8");
+                    response.getWriter().write(jsonObject.toString());
+                }
+                else {
+                    response.setStatus(400);
+                    response.getWriter().write("error");
+                }
+            }
+            else {
+                response.setStatus(400);
+                response.getWriter().write("error");
+            }
+        }
+        else {
+            response.setStatus(400);
+            response.getWriter().write("error");
         }
     }
 
     private String generateMessage(String prompt) throws IOException {
 
-        // Logger.error(" @@@@@@ " + config.getOpaxApiKey() + " @@@@ " +
-        // config.getComponents() + " @@@@@ " + config.getOpenAIAPIKey() + " @@@@ " +
-        // config.getToolbarEnabled());
-        // Generate the chat message using ChatGPT API
         String requestBody = MAPPER.writeValueAsString(new ChatGptRequest(prompt, "gpt-3.5-turbo", "user"));
         HttpPost request = new HttpPost(CHATGPT_API_ENDPOINT);
         request.addHeader("Authorization", "Bearer" + " " + config.getOpaxApiKey());
 
         request.addHeader("Content-Type", "application/json");
         request.setEntity(new StringEntity(requestBody, "UTF-8"));
-        Logger.error("requestBody: " + requestBody);
         HttpResponse response = client.execute(request);
-        Logger.error("response: " + response);
 
-        ChatGptResponse chatGptResponse = MAPPER.readValue(EntityUtils.toString(response.getEntity(), "UTF-8"),
-                ChatGptResponse.class);
-        Logger.error("chatGptResponse: " + chatGptResponse);
-
+        ChatGptResponse chatGptResponse = MAPPER.readValue(EntityUtils.toString(response.getEntity(), "UTF-8"), ChatGptResponse.class);
         String message = chatGptResponse.getChoices().get(0).getMessage().getContent();
-        Logger.error("response message: " + message);
 
         return message;
-
     }
+
 
     private String generatePrompt(String data) {
 
@@ -122,59 +131,39 @@ public class OpaxServlet extends SlingAllMethodsServlet {
             case "content-summarizer":
                 return promptGuide + "Summarize the following content in a " + tone + "tone: " + content;
             case "aida-framework":
-                return promptGuide
-                        + "Never mention the AIDA framework. Write content using the AIDA framework, without mentioning that you are using the AIDA framework - don't mention anything about attention, etc. with the following properties: Company Name: "
-                        + companyName + ", Product: " + product + ", Tone: " + tone + ".";
+                return promptGuide + "Never mention the AIDA framework. Write content using the AIDA framework, without mentioning that you are using the AIDA framework - don't mention anything about attention, etc. with the following properties: Company Name: " + companyName + ", Product: " + product + ", Tone: " + tone + ".";
             case "bab-framework":
-                return promptGuide
-                        + "Write content using the BAB (Before-After-Bridge) framework with the following properties: Company Name: "
-                        + companyName + ", Product: " + product + ", Tone: " + tone + ".";
+                return promptGuide + "Write content using the BAB (Before-After-Bridge) framework with the following properties: Company Name: " + companyName + ", Product: " + product + ", Tone: " + tone + ".";
             case "blog-post-conclusion-paragraph":
-                return promptGuide
-                        + "Write a conclusion paragraph for the following blog post using this post outline: " + outline
-                        + ". Include a call to action: " + cta + ". Use this tone of voice: " + tone + ".";
+                return promptGuide + "Write a conclusion paragraph for the following blog post using this post outline: " + outline + ". Include a call to action: " + cta + ". Use this tone of voice: " + tone + ".";
             case "blog-post-intro-paragraph":
-                return promptGuide + "Write an introduction paragraph for the following blog post title: " + title
-                        + ". The audience is " + audience + ". Use this tone of voice: " + tone + ".";
+                return promptGuide + "Write an introduction paragraph for the following blog post title: " + title + ". The audience is " + audience + ". Use this tone of voice: " + tone + ".";
             case "blog-post-outline":
-                return promptGuide + "Write an outline for the following blog post title: " + title
-                        + ". Use this tone of voice: " + tone + ".";
+                return promptGuide + "Write an outline for the following blog post title: " + title + ". Use this tone of voice: " + tone + ".";
             case "company-bio":
-                return promptGuide + "Write a company bio for the following company name: " + name + ". Company info: "
-                        + info + ". Use this tone of voice: " + tone + ".";
+                return promptGuide + "Write a company bio for the following company name: " + name + ". Company info: " + info + ". Use this tone of voice: " + tone + ".";
             case "content-improver":
-                return promptGuide + "Improve the following content: " + content + ". Use this tone of voice: " + tone
-                        + ".";
+                return promptGuide + "Improve the following content: " + content + ". Use this tone of voice: " + tone + ".";
             case "simplify":
-                return promptGuide + "Explain the following content to a child at grade level " + gradeLevel + ": "
-                        + content + ".";
+                return promptGuide + "Explain the following content to a child at grade level " + gradeLevel + ": " + content + ".";
             case "faq-generator":
-                return promptGuide + "Write " + numQuestions + " FAQs for the following topic: " + topic
-                        + ". Use this tone of voice: " + tone + ".";
+                return promptGuide + "Write " + numQuestions + " FAQs for the following topic: " + topic + ". Use this tone of voice: " + tone + ".";
             case "feature-to-benefit":
-                return promptGuide + "Write the benefits of the features of this product description: " + description
-                        + ". Use this tone of voice: " + tone + ".";
+                return promptGuide + "Write the benefits of the features of this product description: " + description + ". Use this tone of voice: " + tone + ".";
             case "listicle":
-                return promptGuide + "Write a listicle for the following topic: " + topic + ". The list should have "
-                        + listCount + " items. Use this tone of voice: " + tone + ".";
+                return promptGuide + "Write a listicle for the following topic: " + topic + ". The list should have " + listCount + " items. Use this tone of voice: " + tone + ".";
             case "one-shot-blog-post":
-                return promptGuide + "Write a one-shot blog post for the following topic: " + topic
-                        + ". Use this tone of voice: " + tone + ".";
+                return promptGuide + "Write a one-shot blog post for the following topic: " + topic + ". Use this tone of voice: " + tone + ".";
             case "perfect-headline":
-                return promptGuide + "Write a perfect headline for the following product description: "
-                        + productDescription + ". The company product name is " + companyProductName
-                        + ". The customer avatar is " + customerAvatar + ". The customer problem is " + customerProblem
-                        + ". Use this tone of voice: " + tone + ".";
+                return promptGuide + "Write a perfect headline for the following product description: " + productDescription + ". The company product name is " + companyProductName + ". The customer avatar is " + customerAvatar + ". The customer problem is " + customerProblem + ". Use this tone of voice: " + tone + ".";
             case "persuasive-bullet-points":
-                return promptGuide + "Write persuasive bullet points for the following content: Company/Product Name: "
-                        + companyProductName + ", Product Description: " + productDescription + ", Tone: " + tone + ".";
+                return promptGuide + "Write persuasive bullet points for the following content: Company/Product Name: " + companyProductName + ", Product Description: " + productDescription + ", Tone: " + tone + ".";
             case "press-release":
-                return promptGuide + "Write a press release for the following topic: " + topic
-                        + ". The press release should have " + points + " points.";
+                return promptGuide + "Write a press release for the following topic: " + topic + ". The press release should have " + points + " points.";
             case "sentence-expander":
-                return promptGuide + "Expand the following sentence: " + sentence + ". Use this tone of voice: " + tone
-                        + ".";
+                return promptGuide + "Expand the following sentence: " + sentence + ". Use this tone of voice: " + tone + ".";
         }
         return "";
     }
 }
+
